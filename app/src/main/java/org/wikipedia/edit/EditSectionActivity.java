881d3021592d57964aff396d5a2d7c4ef976bf80
Chris Zhao <xinyue.zhao@immonet.de>
2016-12-14 23:31:09 +0100

    Fixed to re-get toolbar(actionbar) in EditSectionActivity
    Change-Id: Ia92b248e795bf25244e15fd2be5087b063dbe2d7

--- app/src/main/java/org/wikipedia/edit/EditSectionActivity.java
+++ app/src/main/java/org/wikipedia/edit/EditSectionActivity.java
@@ -11,6 +11,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
@@ -130,7 +131,8 @@ public class EditSectionActivity extends ThemedActionBarActivity {
         progressDialog.setCancelable(false);
         progressDialog.setMessage(getString(R.string.dialog_saving_in_progress));
 
-        final ActionBar supportActionBar = getSupportActionBar();
+        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
+        ActionBar supportActionBar = getSupportActionBar();
         if (supportActionBar != null) {
             supportActionBar.setTitle("");
         }
